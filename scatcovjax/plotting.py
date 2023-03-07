7b06189256e67b08616566eb9f78137e0584dbd8
mousset <louise.mousset@irap.omp.eu>
2023-03-07 13:44:33 +0100

    move some plot functions from Synthesis_lib

--- scatcovjax/plotting.py
+++ scatcovjax/plotting.py
@@ -158,3 +158,45 @@ def plot_scatcov_coeffs(S1, P00, C01, C11, name=None, hold=True, color='blue', l
     plt.legend()
 
     return
+
+
+# def plot_loss(loss, figsize=(6, 4), fontsize=16, color='b', title=''):
+#     plt.figure(figsize=figsize)
+#     plt.plot(loss, 'o', color=color)
+#     plt.ylabel('Loss', fontsize=fontsize)
+#     plt.yscale('log')
+#     plt.xlabel('Iteration', fontsize=fontsize)
+#     plt.title(title)
+#     plt.grid()
+#     return
+
+
+# def plot_power_spectrum(target_Cl, ini_Cl, synthetic_Cl, J, filter_alm, m_MW, figsize=(10, 8), fontsize=16):
+#     plt.figure(figsize=figsize)
+#     plt.plot(target_Cl, 'b', label='Target')
+#     plt.plot(ini_Cl, 'g', label='Initial')
+#     plt.plot(synthetic_Cl, 'r', label='Synthesis')
+#     colors = cm.get_cmap('viridis', J).colors
+#     for j in range(J):
+#         c = colors[j]
+#         lmin_band, lmax_band = wlib.get_band_axisym_wavelet(filter_alm, m_MW, j_scale=j)
+#         plt.axvline(lmin_band, color=c, ls='--')
+#         plt.axvline(lmax_band, color=c, ls='--')
+#         plt.axvspan(lmin_band, lmax_band, color=c, alpha=0.1)
+#     plt.yscale('log')
+#     plt.xlabel(r'$\ell$', fontsize=fontsize)
+#     plt.ylabel(r'$C_\ell$', fontsize=fontsize)
+#     plt.grid()
+#     plt.legend(fontsize=fontsize)
+#     return
+
+
+# def plot_histo(target_hpx, synthetic_hpx, bins=50, range=(-5, 5), ymax=1300, fontsize=16):
+#     plt.figure(figsize=(10, 8))
+#     plt.hist(target_hpx.ravel(), bins=bins, range=range, color='b', alpha=0.3, label='Target')
+#     plt.hist(synthetic_hpx.ravel(), bins=bins, range=range, color='r', alpha=0.3, label='Synthesis')
+#     plt.legend(fontsize=fontsize)
+#     plt.ylim(0, ymax)
+#     plt.xlim(range)
+#     plt.grid()
+#     return
