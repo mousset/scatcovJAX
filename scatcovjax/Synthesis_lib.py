ad77c95d52fc0a9208c6a84cabc1e09871e555b7
mousset <louise.mousset@irap.omp.eu>
2023-03-08 18:12:47 +0100

    simplify the optax fit function

--- scatcovjax/Synthesis_lib.py
+++ scatcovjax/Synthesis_lib.py
@@ -14,12 +14,12 @@ def chi2(model, data):
     return jnp.sum(jnp.abs(data-model)**2)
 
 
-def fit_brutal(params, loss_func, momentum: float = 2., niter: int = 10):
+def fit_brutal(params, loss_func, momentum: float = 2., niter: int = 10, loss_history: list = None):
     ### Gradient of the loss function
     grad_loss_func = jit(grad(loss_func))
-    # grad_loss_func = grad(loss_func)
 
-    loss_history = []
+    if loss_history is None:
+        loss_history = []
     for i in range(niter):
         start = time.time()
         params -= momentum * np.conj(grad_loss_func(params))
@@ -32,23 +32,23 @@ def fit_brutal(params, loss_func, momentum: float = 2., niter: int = 10):
     return params, loss_history
 
 
-def fit_optax(params: optax.Params, optimizer: optax.GradientTransformation, loss_func, niter : int =10) -> optax.Params:
+def fit_optax(params: optax.Params, optimizer: optax.GradientTransformation, loss_func,
+              niter: int = 10, loss_history: list = None) -> optax.Params:
+    ### Gradient of the loss function
+    grad_func = jit(grad(loss_func))
+
+    if loss_history is None:
+        loss_history = []
     opt_state = optimizer.init(params)
-
-    @jax.jit
-    def step(params, opt_state):
-        loss_value, grads = jax.value_and_grad(loss_func)(params)
-        updates, opt_state = optimizer.update(grads, opt_state, params)
-        params = optax.apply_updates(params, updates)
-        return params, opt_state, loss_value
-
-    loss_history = []
     for i in range(niter):
         start = time.time()
-        params, opt_state, loss_value = step(params, opt_state)
+        grads = jnp.conj(grad_func(params))  # Take the conjugate of the gradient
+        updates, opt_state = optimizer.update(grads, opt_state, params)
+        params = optax.apply_updates(params, updates)
+        end = time.time()
         if i % 1 == 0:
+            loss_value = loss_func(params)
             loss_history.append(loss_value)
-            end = time.time()
             print(f'Iter {i}, Loss: {loss_value:.5f}, Time = {end - start:.5f} s/iter')
 
     return params, loss_history
